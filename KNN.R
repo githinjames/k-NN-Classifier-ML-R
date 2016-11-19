f55631fd37bd6e4403bdd11ef7b2a1bcb148e786
Githin James <githinjames@gmail.com>
2016-11-19 23:44:39 +0530

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -22,5 +22,6 @@ iris_test_target <- iris3$Species[131:150]
 library(class)
 sqrt(150)  # 12.24754, take 13 as k value, odd number to break the tie
 m1 <- knn(train = iris_train, test = iris_test, cl= iris_train_target, k=13)
-summary(m1)
 m1
+table(iris_test_target, m1)
+## CODE ENDS HERE
