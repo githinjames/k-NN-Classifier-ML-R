80deb8e8bde86cc0aa9e5ed5737cb65c1bdbc3bb
Githin James <githinjames@gmail.com>
2016-11-19 23:43:03 +0530

    Add files via upload

---
+++ KNN.R
@@ -0,0 +1,26 @@
+# Using K-NN Algorithm for classifying the Flower Species in IRIS Dataset
+data("iris")
+str(iris)
+table(iris$Species)
+head(iris)
+# Making Training and validation dataset
+set.seed(1234)
+g<- runif(nrow(iris))
+iris3 <- iris[order(g),]
+head(iris3)
+# Normalization function
+normalize <- function(x)
+{
+  return((x-min(x))/(max(x)-min(x)))
+}
+iris_n <- as.data.frame(lapply(iris3[,c(1,2,3,4)], normalize))
+summary(iris_n)
+iris_train <- iris_n[1:130,]
+iris_test <- iris_n[131:150,]
+iris_train_target <- iris3$Species[1:130]
+iris_test_target <- iris3$Species[131:150]
+library(class)
+sqrt(150)  # 12.24754, take 13 as k value, odd number to break the tie
+m1 <- knn(train = iris_train, test = iris_test, cl= iris_train_target, k=13)
+summary(m1)
+m1
